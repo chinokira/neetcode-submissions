64538c6af638c2851fd5612633f83a230c9ffc24
chinokira <90824558+chinokira@users.noreply.github.com>
2026-07-11 12:47:33 +0200

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+
+        while (l <= r) {
+            int index = (r-l)/2+l;
+            if(nums[index] > target){
+                r = index -1;
+            } else if(nums[index] < target) {
+                l = index + 1;
+            } else {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+}
