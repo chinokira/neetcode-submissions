7082d8d117a48628f93e478c264cb790a5628a3c
chinokira <90824558+chinokira@users.noreply.github.com>
2026-07-25 22:15:14 +0200

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+
+        int[] arr = new int[nums.length];
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] >= 0){
+                if(index > 0){
+                    if(arr[index-1] != nums[i])
+                        arr[index++] = nums[i];
+                    }
+                    else 
+                        arr[index++] = nums[i];
+                }
+            }
+        for(int i : arr){
+            System.out.println(i);
+        }
+
+        if(arr[0] > 1)
+            return 1;
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i]-1 != arr[i-1])
+                return arr[i-1]+1;
+        }
+
+        return arr[arr.length-1]+1;
+    }
+}
