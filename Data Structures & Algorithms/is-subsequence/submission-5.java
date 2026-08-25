78aad035915ec1af6d4d2e96569cabd9d6d204a7
chinokira <90824558+chinokira@users.noreply.github.com>
2026-08-25 10:44:42 +0200

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() > t.length()) 
+            return false;
+        if(s.isEmpty() || s.isBlank())
+            return true;
+        int r = 0;
+        for(int i = 0; i < t.length() && r < s.length(); i++){
+            if(t.charAt(i) == s.charAt(r)){
+                r++;
+            }
+        }
+
+        return r == s.length();
+    }
+}
