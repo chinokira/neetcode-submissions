6013ece2e63bd99519800cab0b32b21bf907a8d2
chinokira <90824558+chinokira@users.noreply.github.com>
2026-08-25 12:33:04 +0200

    Add: number-of-senior-citizens - submission-10

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-10.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int num = 0;
+        for(String s : details){
+            if(s.charAt(s.length()-4) > '6' || s.charAt(s.length()-4) == '6' && s.charAt(s.length()-3) >= '1')
+                num++;
+            
+        }
+        return num;
+    }
+}
